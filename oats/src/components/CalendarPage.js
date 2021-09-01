3f600208d8c271e4d444ff6661dd3e769e8e61fd
henchaa9 <bonis.obo@gmail.com>
2021-09-01 19:38:18 +0300

    started work on calendar page

--- oats/src/components/CalendarPage.js
+++ oats/src/components/CalendarPage.js
@@ -4,14 +4,28 @@ import "react-calendar/dist/Calendar.css";
 
 const CalendarPage = () => {
   const [date, setDate] = useState(new Date());
+  const [showDate, setShowDate] = useState("");
 
   const clickDate = (date) => {
     setDate(date);
+
+    if (new Date().getFullYear() < date.getFullYear()) {
+      setShowDate(
+        date.getDate() + "." + date.getMonth() + "." + date.getFullYear()
+      );
+    } else {
+      setShowDate(date.getDate() + "." + date.getMonth());
+    }
   };
 
   return (
     <div className="calendar-main">
-      <Calendar value={date} onChange={clickDate} />
+      <div className="calendar-div">
+        <Calendar value={date} onChange={clickDate} />
+      </div>
+      <div className="day-div">
+        <h1>{showDate}</h1>
+      </div>
     </div>
   );
 };
