483b25ebbb6452db8100b98ad237cc2884f829bc
henchaa9 <bonis.obo@gmail.com>
2021-09-24 20:48:03 +0300

    continued working on calendar page

--- oats/src/components/AddExercises.js
+++ oats/src/components/AddExercises.js
@@ -1,11 +1,45 @@
-import React from "react";
+import { React, useState, useEffect } from "react";
 
 const AddExercises = () => {
-  return (
-    <div>
-      <h1>here you add exercises</h1>
-    </div>
-  );
+  const [exercises, setExercises] = useState(null);
+
+  const fetchAndSet = () =>
+    fetch("http://localhost:8000/exercises")
+      .then((res) => {
+        return res.json();
+      })
+      .then((data) => {
+        setExercises(data);
+      });
+
+  useEffect(() => {
+    fetchAndSet();
+  }, []);
+
+  if (exercises) {
+    return (
+      <div>
+        <div className="available-exercises">
+          <ul className="smallList">
+            {exercises.map((exercise) => {
+              return (
+                <li
+                  key={exercise.id}
+                  onClick={() => {
+                    console.log("clicked");
+                  }}
+                >
+                  {exercise.name}
+                </li>
+              );
+            })}
+          </ul>
+        </div>
+      </div>
+    );
+  } else {
+    return <div className="loading">Loading...</div>;
+  }
 };
 
 export default AddExercises;
